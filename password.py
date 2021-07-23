3a6b5f55d39bde20801b89b229c75b3d1d123c06
bill <wcstrickland1@gmail.com>
2021-07-22 22:43:38 -0500

    formatting and initial setup case handled

--- password.py
+++ password.py
@@ -22,7 +22,7 @@ def db_connect(params):
         cur = conn.cursor()
         cur.execute("SELECT version()")
         print(cur.fetchone())
-        print("Database connected:")
+        print("\nDatabase connected!\n")
     except (Exception, psycopg2.DatabaseError) as error:
         print("error connecting to db : ", error)
     return cur, conn
@@ -57,7 +57,7 @@ def get_password(master_password, service):
         rows = cur.fetchall()
         for row in rows:
             clear_pass = decrypt_pass(master_password, bytes(row[2]))
-            clear_pass = str(clear_pass)
+            clear_pass = clear_pass.decode('UTF-8')
             print(f"service: {row[0]} username: {row[1]}\nPASSWORD: {clear_pass}")
     except (Exception, psycopg2.DatabaseError) as error:
         print("error retrieving password table: ", error)
@@ -71,6 +71,7 @@ def add_password():
     try:
         cur.execute("insert into password(service, username, password) values (%s, %s, %s)", (service, username, password ))
         conn.commit()
+        print("\nPassword inserted successfully!\n")
     except (Exception, psycopg2.DatabaseError) as error:
         print("error adding new passord: ", error)
     
@@ -98,33 +99,56 @@ if __name__ == '__main__':
 
     db_opts["password"] = getpass.getpass("Enter database password: ")
     cur, conn = db_connect(db_opts)
-    master_password = getpass.getpass("Enter your master password: ")
+
+    first_time = False
+
+    try:
+        cur.execute("select * from password")
+    except:
+        first_time = True
+        pass
+
+    if not first_time:
+        master_password = getpass.getpass("\nEnter your master password: ")
+    else:
+        master_password = getpass.getpass("\nEnter the master password you will use from now on : ")
 
     hassher = hashlib.sha256()
     bytes_passwd = master_password.encode('UTF-8')
     hassher.update(bytes_passwd)
     hashed_passwd = hassher.digest()
     encryptor = AES.new(hashed_passwd, AES.MODE_CFB, 'This is an IV456')
-    
+
+    first_time = False
+
+    try:
+        cur.execute("select * from password")
+    except:
+        first_time = True
+        pass
+
+    conn.commit()
 
     try:
         cur.execute("create table password (service varchar(255), username varchar(255), password bytea )")
         sanity_check = ciphertext = encryptor.encrypt("sanity_check")
-        cur.execute("insert into password (service, username, password) values (%s, %s, %s)", ("row1", "row1", sanity_check))
+        cur.execute("insert into password (service, username, password) values (%s, %s, %s)", ("1d8c21bb-eba2-45bc-b3c0-f790c3c0c334", "1d8c21bb-eba2-45bc-b3c0-f790c3c0c334", sanity_check))
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
-        print("error creating password table: ", error)
+        pass
     
     conn.commit()
 
     cur.execute("select * from password")
     first_row_check = cur.fetchone()
     check_sanity = decrypt_pass(hashed_passwd, bytes(first_row_check[2]))
-    print(check_sanity)
     if check_sanity != b'sanity_check':
-        print("fail")
+        print("*"*40)
+        print("\nThat is not the password!\n")
+        print("*"*40)
+        quit()
     else:
-        print("success")
+        print("\nSuccess!!!\n")
     
 
     
@@ -137,7 +161,7 @@ if __name__ == '__main__':
     add a new password: 3
     quit: 0
     """)
-        user_input = input()
+        user_input = input("\t: ")
         if user_input not in valid_choices:
             print("That is not valid input\n")
             continue
@@ -151,3 +175,5 @@ if __name__ == '__main__':
         if user_input == "3":
             add_password()
             
+    cur.close()
+    conn.close()
